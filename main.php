f7e8fa5aea667f4ac6e4c7e1794d92855a8b396f
Keang NAGE <knage@e5r11p9.jnb.42.fr>
2016-09-28 12:50:53 +0200

    Data saving

--- main.php
+++ main.php
@@ -3,8 +3,34 @@
 	$rules = array();
 	$ifacts;
 	$query;
-	
-
+	$alpha = array(
+			'A' => false,
+			'B' => false,
+			'C' => false,
+			'D' => false,
+			'E' => false,
+			'F' => false,
+			'G' => false,
+			'H' => false,
+			'I' => false,
+			'J' => false,
+			'K' => false,
+			'L' => false,
+			'M' => false,
+			'N' => false,
+			'O' => false,
+			'P' => false,
+			'Q' => false,
+			'R' => false,
+			'S' => false,
+			'T' => false,
+			'U' => false,
+			'V' => false,
+			'W' => false,
+			'X' => false,
+			'Y' => false,
+			'Z' => false, 
+	);
 	require('error_test.php');
 
 	if (isset($argv[1]))
